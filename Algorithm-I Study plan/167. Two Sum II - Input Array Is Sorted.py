fff986ae730194f00b83423b2fe2fc0ab2313368
Siva Balan <sivabalan.arun@gmail.com>
2022-07-24 21:11:16 +0530

    Create 167. Two Sum II - Input Array Is Sorted.py

---
+++ Algorithm-I Study plan/167. Two Sum II - Input Array Is Sorted.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        i,j = 0,len(numbers) -1
+        arr =[]
+        while i<j :
+            if numbers[i] + numbers[j] == target:
+                arr.append(i+1)
+                arr.append(j+1)
+            if numbers[i] + numbers[j] < target:
+                i += 1
+            else:
+                j -=1
+        return arr
