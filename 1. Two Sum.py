ab7e8a7f40f6002687e81f5628de1b863155a646
Siva Balan <sivabalan.arun@gmail.com>
2022-07-24 21:49:43 +0530

    Update 1. Two Sum.py

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -5,3 +5,16 @@ class Solution:
                 if target == nums[i] + nums[j]:
                     return [i,j]
            
+# optimized solution
+    
+    class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        s = {}
+        for j in range(len(nums)):
+            if target - nums[j] in s:
+                return [j,s[target - nums[j]]]
+            s[nums[j]] = j 
+    
+# Runtime: 83 ms, faster than 75.87% of Python3 online submissions for Two Sum.
+# Memory Usage: 15.2 MB, less than 50.46% of Python3 online submissions for Two Sum.
