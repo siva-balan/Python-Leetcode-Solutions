60b276d5729fbeabc51aec4df98eb7dd40512043
Siva Balan <sivabalan.arun@gmail.com>
2022-09-21 00:13:43 +0530

    Create 718. Maximum Length of Repeated Subarray.py

---
+++ September_daily_ques/718. Maximum Length of Repeated Subarray.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findLength(self, nums1: List[int], nums2: List[int]) -> int:
+        
+        n,m = len(nums1),len(nums2)
+        
+        dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
+        #output = 0
+        
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if nums1[i-1] == nums2[j-1]:
+                    dp[i][j] = 1+ dp[i-1][j-1]
+                
+        return max(max(row) for row in dp)
