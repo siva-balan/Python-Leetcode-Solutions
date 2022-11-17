01fd16ba67a97990aad63056a9facde87d7bd471
Siva Balan <sivabalan.arun@gmail.com>
2022-11-18 01:59:09 +0530

    Create 223. Rectangle Area.py

---
+++ November Daily Questions/223. Rectangle Area.py
@@ -0,0 +1,6 @@
+class Solution:
+    def computeArea(self, ax1: int, ay1: int, ax2: int, ay2: int, bx1: int, by1: int, bx2: int, by2: int) -> int:
+        first = (ax2 - ax1) * (ay2 - ay1)
+        second = (bx2 - bx1) * (by2 - by1)
+        overlap = max(min(ax2, bx2) - max(ax1, bx1), 0) * max(min(ay2, by2) - max(ay1, by1), 0)
+        return first + second - overlap
