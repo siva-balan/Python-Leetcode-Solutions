6b3dce343a39f9d18a4ec79b2543b7e85ce0a4e0
Siva Balan <sivabalan.arun@gmail.com>
2022-12-12 11:47:56 +0530

    Create 70. Climbing Stairs.py

---
+++ December Questions/70. Climbing Stairs.py
@@ -0,0 +1,9 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        
+        first,second = 1,1
+
+        for _ in range(n):
+
+            first,second = second, first+ second
+        return first
