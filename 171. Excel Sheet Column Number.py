ab39838d79fd1f3d368d7a4cf0b8d96b5589f716
Siva Balan <sivabalan.arun@gmail.com>
2023-01-10 18:18:30 +0530

    Delete 171. Excel Sheet Column Number.py

--- 171. Excel Sheet Column Number.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def titleToNumber(self, columnTitle: str) -> int:
-
-        n = len(columnTitle)
-        output = 0
-        while n > 0:
-            output += (26**(n-1))*(ord(columnTitle[len(columnTitle)-n]) - 64)
-            n -=1
-        return output
