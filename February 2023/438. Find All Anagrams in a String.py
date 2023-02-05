1228c141799c51b1eb267ed03ff2aaa3d64a8452
Siva Balan <sivabalan.arun@gmail.com>
2023-02-05 21:24:17 +0530

    Create 438. Find All Anagrams in a String.py

---
+++ February 2023/438. Find All Anagrams in a String.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        
+        if len(p) > len(s): return []
+        pCounter = {}
+        sCounter = {}
+        
+        for i in range(len(p)):
+            if p[i] not in pCounter:
+                pCounter[p[i]] = 1
+            else:
+                pCounter[p[i]] += 1
+            sCounter[s[i]] = 1 + sCounter.get(s[i], 0)
+        
+        res = [0] if pCounter == sCounter else []
+        
+        l = 0
+        for x in range(len(p), len(s)):
+            sCounter[s[x]] = 1 + sCounter.get(s[x], 0)
+            sCounter[s[l]] -= 1
+            
+            if sCounter[s[l]] == 0:
+                sCounter.pop(s[l])
+            l += 1
+            
+            if sCounter == pCounter:
+                res.append(l)
+        return res
