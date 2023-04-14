86c7b5b738e7caba29805b5cef63ceb49a06c67e
Siva Balan <sivabalan.arun@gmail.com>
2023-04-14 23:20:18 +0530

    Create 516. Longest Palindromic Subsequence.py

---
+++ April_2023/516. Longest Palindromic Subsequence.py
@@ -0,0 +1,22 @@
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+
+        @lru_cache(None)
+
+        def dp(i,j):
+            if i == j:
+                return 1
+            elif i > j:
+                return 0
+
+            if arr[i] == arr[j]:
+                val = 2 + dp(i+1,j-1)
+                return val
+            else:
+                val = max(dp(i+1,j),dp(i,j-1))
+                return val
+        
+        arr = list(s)
+
+        return dp(0,len(arr)-1)
+        
